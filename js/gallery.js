2cf2690ccd4b060ee3cbfd71195091ad7c92c7d4
Paul Christian Tan <paulchristiantan.projects@gmail.com>
2025-08-10 13:05:39 +0800

    code clean up

--- js/gallery.js
+++ js/gallery.js
@@ -19,8 +19,8 @@ const images = [
     "assets/img/gallery/angela6.jpg", "assets/img/gallery/charice6.jpg", "assets/img/gallery/alexa6.jpg", 
     "assets/img/gallery/sophia6.jpg", "assets/img/gallery/charlotte6.jpg", "assets/img/gallery/kaia3.jpg", 
     
-    "assets/img/gallery/angela7.jpg", "assets/img/gallery/charice7.jpg", "assets/img/gallery/alexa7.jpg", 
-    "assets/img/gallery/sophia7.jpg", "assets/img/gallery/charlotte7.jpg", "assets/img/gallery/kaia3.jpg", 
+    // "assets/img/gallery/angela7.jpg", "assets/img/gallery/charice7.jpg", "assets/img/gallery/alexa7.jpg", 
+    // "assets/img/gallery/sophia7.jpg", "assets/img/gallery/charlotte7.jpg", "assets/img/gallery/kaia3.jpg", 
 
     "assets/img/gallery/angela9.jpg", "assets/img/gallery/charice9.jpg", "assets/img/gallery/alexa9.jpg", 
     "assets/img/gallery/sophia9.jpg", "assets/img/gallery/charlotte9.jpg", "assets/img/gallery/kaia9.jpg", 
--- js/scripts.js
+++ js/scripts.js
@@ -36,40 +36,40 @@ window.addEventListener('DOMContentLoaded', event => {
         const scrollToTop = document.body.querySelector('.scroll-to-top');
         if (document.documentElement.scrollTop > 100) {
             if (!scrollToTopVisible) {
-                fadeIn(scrollToTop);
+                // fadeIn(scrollToTop);
                 scrollToTopVisible = true;
             }
         } else {
             if (scrollToTopVisible) {
-                fadeOut(scrollToTop);
+                // fadeOut(scrollToTop);
                 scrollToTopVisible = false;
             }
         }
     })
 })
 
-function fadeOut(el) {
-    el.style.opacity = 1;
-    (function fade() {
-        if ((el.style.opacity -= .1) < 0) {
-            el.style.display = "none";
-        } else {
-            requestAnimationFrame(fade);
-        }
-    })();
-};
+// function fadeOut(el) {
+//     el.style.opacity = 1;
+//     (function fade() {
+//         if ((el.style.opacity -= .1) < 0) {
+//             el.style.display = "none";
+//         } else {
+//             requestAnimationFrame(fade);
+//         }
+//     })();
+// };
 
-function fadeIn(el, display) {
-    el.style.opacity = 0;
-    el.style.display = display || "block";
-    (function fade() {
-        var val = parseFloat(el.style.opacity);
-        if (!((val += .1) > 1)) {
-            el.style.opacity = val;
-            requestAnimationFrame(fade);
-        }
-    })();
-};
+// function fadeIn(el, display) {
+//     el.style.opacity = 0;
+//     el.style.display = display || "block";
+//     (function fade() {
+//         var val = parseFloat(el.style.opacity);
+//         if (!((val += .1) > 1)) {
+//             el.style.opacity = val;
+//             requestAnimationFrame(fade);
+//         }
+//     })();
+// };
 
 // Loader
 
